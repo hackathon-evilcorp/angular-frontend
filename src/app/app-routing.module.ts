d12c47e3f55cc92a014684fc6727e94320f1b733
nagypeti <nagypeti222@gmail.com>
2019-03-28 21:36:04 +0100

    Aligned delete and edit buttons on postits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { FridgeComponent } from './components/fridge/fridge.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './guard/auth.guard';
 import { CreateFridgeComponent } from './components/create-fridge/create-fridge.component';
+import { CreatePostitComponent } from './components/create-postit/create-postit.component';
 
 
 const routes: Routes = [
   { path: 'fridge/:id', component: FridgeComponent},
   { path: 'addfridge', component: CreateFridgeComponent},
+  { path: 'addpostit', component: CreatePostitComponent},
+  { path: 'fridge/edit/postit/:id', component: CreatePostitComponent},
+  //{ path: 'fridge/delete/postit/:id', component: DeletePostitComponent},
   { path: 'login', component: LoginComponent},
   { path: '**', redirectTo: '' }
 ];
@@ -18,5 +22,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [FridgeComponent, LoginComponent]
+export const routingComponents = [FridgeComponent, LoginComponent, CreatePostitComponent]
 
