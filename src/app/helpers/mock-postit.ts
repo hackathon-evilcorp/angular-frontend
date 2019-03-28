44e3e5efad08d01b3c7c3d6a25261e779baf6376
Mihály Barczi <barczi.mihaly@gmail.com>
2019-03-28 19:55:04 +0100

    add two more postit mocks

--- src/app/helpers/mock-postit.ts
+++ src/app/helpers/mock-postit.ts
@@ -2,5 +2,7 @@ import { PostIt } from '../models/postIt';
 
 export const MOCK_POSTIT: PostIt[] = [
     new PostIt(1, null, "shopitem", "8 milk", null),
-    new PostIt(2, null, "message", "mosogass", null)
+    new PostIt(2, null, "message", "mosogass el", null),
+    new PostIt(3, null, "shopitem", "2kg lilahagyma", null),
+    new PostIt(4, null, "shopitem", "1 zsák kutyakaja", null)
 ];
